fe5ad93108445b2cfef67c39abd0c7b17640bfcd
WhyY0u <aimsi13579@gmail.com>
2024-09-29 21:10:14 -0400

    upd Docker

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-
 const User = require('./model/userModel')
 
 const middleware = require('./middleware/middleware');
@@ -12,7 +11,12 @@ const userRouter = require("./router/userRouter");
 const confidantRouter = require('./router/confidantRouter');
 const imageRouter = require('./router/imageRouter');
 
-mongoose.connect('mongodb://localhost:27017/kit');
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+mongoose.connect(`mongodb://${dbUser}:${dbPassword}@localhost:27017/${dbName}`);
 
 const app = express();
 app.use(bodyParser.json());
---
+++ docker/express.dockerfile
@@ -0,0 +1,18 @@
+FROM node:22
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y git
+
+RUN git clone https://github.com/Nadgob228/College-Telegram-App.git .
+
+RUN rm -rf frontend
+RUN rm -rf docker
+
+WORKDIR /app/backend
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start"]
