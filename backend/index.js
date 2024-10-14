b8f046df1fb0514cd8156f3ecc36e99ee41bc512
WhyY0u <aimsi13579@gmail.com>
2024-10-14 19:52:41 -0400

    fix

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const User = require('./model/userModel');
+
 const cors = require('cors');
 
 
--- docker/react.dockerfile
+++ docker/react.dockerfile
@@ -20,6 +20,6 @@ COPY --from=build /app/frontend/dist /usr/share/nginx/html
 
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
-EXPOSE 5173
+EXPOSE 27017
 
 CMD ["nginx", "-g", "daemon off;"]
